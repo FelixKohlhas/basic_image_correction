2e7fceb85b589c6f101e1b91939d548fff5295c1
FelixKohlhas <18424307+FelixKohlhas@users.noreply.github.com>
2024-02-26 12:02:41 +0100

    feat: limit number of concurrently loaded images

--- basic_image_correction.py
+++ basic_image_correction.py
@@ -34,18 +34,8 @@ def load_config(config_path):
         config = yaml.safe_load(config_file) or {}
     return config
 
-def load_images(directory_path, file_names):
-    images = [io.imread(os.path.join(directory_path, file_name)) for file_name in file_names]
-    return np.stack(images, axis=0)
-
-def save_images(directory_path, file_names, images):
-    for file_name, image in zip(file_names, images):
-        file_path = os.path.join(directory_path, file_name)
-        image = np.clip(image, result_min, result_max).astype(result_dtype)
-        io.imsave(file_path, image, check_contrast=False)
-
 def save_flatfield(directory_path_flatfield, name, flatfield, score):
-    image_path_flatfield = os.path.join(directory_path_flatfield, f"{'_'.join(name)}.png")
+    image_path_flatfield = os.path.join(directory_path_flatfield, f"{name}.png")
     io.imsave(image_path_flatfield, (flatfield * 128).astype(np.uint8), check_contrast=False)
     return
     plt.figure()
@@ -55,13 +45,6 @@ def save_flatfield(directory_path_flatfield, name, flatfield, score):
     plt.tight_layout()
     plt.savefig(image_path_flatfield)
     plt.close()
-
-def process_images_in_batches(images, batch_size, process_function):
-    if batch_size == 0:
-        batch_size = len(images)
-    for i in range(0, len(images), batch_size):
-        # print(f'Processing batch: {i} - {i + batch_size}')
-        yield process_function(images[i:min(i + batch_size, len(images))])
 
 def main():
     args = parse_arguments()
@@ -92,24 +75,58 @@ def main():
     images = pd.DataFrame(image_dicts)
     images_grouped = images.groupby(groupby)
     
-    print(f"{'_'.join(groupby)} (Number of images): Score")
     for name, group in images_grouped:
-        print(f"{'_'.join(name)} ({len(group)})", end=': ', flush=True)
+        if isinstance(name, tuple):
+            name = '_'.join(name)
 
+        log_prefix = f"[{name}] "
+
+        print(log_prefix + f"Number of images: {len(group)}")
+        
         file_names = list(group['_filename'])
-        images = load_images(args.directory_path, file_names)
 
         basic = BaSiC()
         basic.max_workers = workers
 
-        basic.fit(images)
-        print(f'{basic.reweight_score:.4f}')
+        if batch_size == 0:
+            batch_size = len(file_names)
 
-        process_function = lambda batch: basic.transform(batch)
-        batches = process_images_in_batches(images, batch_size, process_function)
+        for i in range(0, len(file_names), batch_size):
+            start = i
+            end = min(i + batch_size, len(file_names))
+            batch_file_names = file_names[start:end]
+            print(log_prefix + f'Fitting batch: {start} - {end}', flush=True)
 
-        for batch_result in batches:
-            save_images(args.directory_path_out, file_names, batch_result)
+            images = []
+            for file_name in batch_file_names:
+                file_path = os.path.join(args.directory_path, file_name)
+                images.append(io.imread(file_path))
+            images = np.stack(images, axis=0)
+
+            basic.fit(images)
+
+        print(log_prefix + f'Score: {basic.reweight_score:.4f}', flush=True)
+
+        for i in range(0, len(file_names), batch_size):
+            start = i
+            end = min(i + batch_size, len(file_names))
+            batch_file_names = file_names[start:end]
+            print(log_prefix + f'Transforming batch: {start} - {end}', flush=True)
+
+            # If batch is whole dataset, no need to load images again
+            if not batch_size == len(file_names):
+                images = []
+                for file_name in batch_file_names:
+                    file_path = os.path.join(args.directory_path, file_name)
+                    images.append(io.imread(file_path))
+                images = np.stack(images, axis=0)
+
+            batch_result = basic.transform(images)
+
+            for file_name, image in zip(batch_file_names, batch_result):
+                file_path = os.path.join(args.directory_path_out, file_name)
+                image = np.clip(image, result_min, result_max).astype(result_dtype)
+                io.imsave(file_path, image, check_contrast=False)
 
         save_flatfield(args.directory_path_flatfield, name, basic.flatfield, basic.reweight_score)
 
